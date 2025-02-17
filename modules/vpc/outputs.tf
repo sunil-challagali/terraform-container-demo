21f962c7f152c49ae60f2fb2a28352a686f1f58f
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:31:53 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,1 +1,20 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet1_id" {
+  value = aws_subnet.subnet1.id
+}
+
+output "subnet2_id" {
+  value = aws_subnet.subnet2.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.main.id
+}
 
