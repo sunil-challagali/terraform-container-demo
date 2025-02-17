d66dfdbff3548a95a63935a9532d02a7cd2985c4
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 12:05:22 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,11 +9,11 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "subnets" {
   count                   = 2
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = element(split(".", var.cidr_block), 0) + "." + element(split(".", var.cidr_block), 1) + "." + count.index + ".0/24"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.vpc_name + "-subnet-${count.index + 1}"
+    Name = "${var.vpc_name}-subnet-${count.index + 1}"
   }
 }
 
