8bcb7e91816b8c2ca242b91ebb35b23772cd50ec
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:55:44 +0530

    Update variables.tf

--- modules/security_groups/variables.tf
+++ modules/security_groups/variables.tf
@@ -1,1 +1,20 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "The name of the security group"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "The ingress rules for the security group"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
 
