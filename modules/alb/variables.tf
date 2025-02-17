1892fcef7ddfbc91f1126aed9230758b72408ba9
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:57:10 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,1 +1,13 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
 
+variable "subnet_ids" {
+  description = "The IDs of the subnets"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group"
+  type
