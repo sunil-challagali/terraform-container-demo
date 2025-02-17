4fa5688e8188e6430de65e924caecf48e6104ef8
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:31:35 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,25 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr1" {
+  description = "The CIDR block for subnet 1"
+  type        = string
+}
+
+variable "subnet_cidr2" {
+  description = "The CIDR block for subnet 2"
+  type        = string
+}
+
+variable "availability_zone1" {
+  description = "The availability zone for subnet 1"
+  type        = string
+}
+
+variable "availability_zone2" {
+  description = "The availability zone for subnet 2"
+  type        = string
+}
 
