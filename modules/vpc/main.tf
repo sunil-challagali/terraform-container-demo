858c390184116609477f39c1c8961a2dc6c1c20b
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 15:27:44 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,6 +6,33 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_route_table_association" "this" {
+  count = length(aws_subnet.subnets)
+  subnet_id = element(aws_subnet.subnets[*].id, count.index)
+  route_table_id = aws_route_table.this.id
+}
+
 resource "aws_subnet" "subnets" {
   count                   = 2
   vpc_id                  = aws_vpc.this.id
