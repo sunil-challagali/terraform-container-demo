75c010cd4951b12ccf32b7f42f3d8c5f73e4d0f3
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:55:29 +0530

    Update main.tf

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -1,28 +1,23 @@
-resource "aws_security_group" "ecs_sg" {
-  vpc_id = var.vpc_id
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = "Security group for my app"
+  vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
   tags = {
-    Name = "ecs_sg"
+    Name = var.sg_name
   }
+}
+
+output "sg_id" {
+  value = aws_security_group.this.id
 }
