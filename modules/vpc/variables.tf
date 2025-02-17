6c767f906cbda622587af4f10e7f34cb4648938e
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 15:17:55 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,9 @@ variable "cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
 }
+
+variable "availability_zones" {
+  description = "The availability zones for the subnets"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
