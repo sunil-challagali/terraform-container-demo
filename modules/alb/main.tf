46ca6d2842147102fe4fc3a3896a3b4da4b2fdd4
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:56:42 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,1 +1,43 @@
+resource "aws_lb" "this" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
+
+  tags = {
+    Name = var.alb_name
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = var.alb_name + "-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+output "dns_name" {
+  value = aws_lb.this.dns_name
+}
 
