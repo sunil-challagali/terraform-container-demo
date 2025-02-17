e5ed5db2e490e254c47e4be64ddf7bcde34fa6cb
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:33:19 +0530

    Update main.tf

--- modules/ecs_service/main.tf
+++ modules/ecs_service/main.tf
@@ -1,1 +1,35 @@
+resource "aws_ecs_task_definition" "task" {
+  family                   = var.task_family
+  container_definitions    = var.container_definitions
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+}
+
+resource "aws_ecs_service" "service" {
+  name            = var.service_name
+  cluster         = var.cluster_id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = var.container_name
+    container_port   = var.container_port
+  }
+
+  depends_on = [aws_lb_listener.front_end]
+}
 
