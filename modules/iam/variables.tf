780239be8354e5afdd19fe4e65fe2df5f3053546
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 11:02:07 +0530

    Create variables.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "execution_role_name" {
+  description = "The name of the IAM execution role"
+  type        = string
+}
+
+variable "task_role_name" {
+  description = "The name of the IAM task role"
+  type        = string
+}
