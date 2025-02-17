86e2d7e59a0289ca29f90974a54ae799e4409949
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 10:54:53 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,61 +1,26 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
 
   tags = {
-    Name = "main_vpc"
+    Name = var.vpc_name
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main_igw"
-  }
-}
-
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr1
-  availability_zone = var.availability_zone1
+resource "aws_subnet" "subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = element(split(".", var.cidr_block), 0) + "." + element(split(".", var.cidr_block), 1) + "." + count.index + ".0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet1"
+    Name = var.vpc_name + "-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr2
-  availability_zone = var.availability_zone2
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet2"
-  }
+output "vpc_id" {
+  value = aws_vpc.this.id
 }
 
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "main_rt"
-  }
+output "subnet_ids" {
+  value = aws_subnet.subnets[*].id
 }
-
-resource "aws_route_table_association" "subnet1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_route_table_association" "subnet2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.main.id
-}
-
